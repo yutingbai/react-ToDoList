2f132bdd8d1f5adfef9b49adac792f54fa80dd82
yutingbai <827045918@qq.com>
2020-01-20 17:31:38 +0800

    Initial the page of project

---
+++ precache-manifest.ccba4f844a8e6d0917e80563c6a4090a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb060c4b9fc1b23afec946a19f61afc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ea884a1f0b60be46935",
+    "url": "/static/css/main.6946d54d.chunk.css"
+  },
+  {
+    "revision": "fe96dfb75ed4861cfb71",
+    "url": "/static/js/2.b4b8cfdd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b4b8cfdd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ea884a1f0b60be46935",
+    "url": "/static/js/main.9cf5ad8d.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
